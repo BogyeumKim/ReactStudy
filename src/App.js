1d68f0bb138980edcba8c650893121a6392c2622
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2021-05-27 11:28:48 +0900

    [UPDATE]리액트 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const name = 'react';
+  const style = {
+    backgroundColor:'black',
+    color: 'aqua',
+    fontSize : 24,
+    padding:'1rem'
+  };
+  return (  
+      <>
+        <Hello 
+          // 이런식으로 작성하는 주석은 안나타남
+        />
+
+        {/* 태그밖에서의 주석은 이렇게 */}
+        
+        <div style={style}>{name}</div>
+        <div className="gray-box"></div>
+      </>
+     
   );
 }
 
