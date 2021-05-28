3108026ba479338285f78e8341e190c66c58d733
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2021-05-28 11:28:28 +0900

    Props
    컴포넌트안에 프로포스를 바로넣기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
+import Wrapper from './Wrapper';
+
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor:'black',
-    color: 'aqua',
-    fontSize : 24,
-    padding:'1rem'
-  };
-  return (  
-      <>
-        <Hello 
-          // 이런식으로 작성하는 주석은 안나타남
-        />
 
-        {/* 태그밖에서의 주석은 이렇게 */}
-        
-        <div style={style}>{name}</div>
-        <div className="gray-box"></div>
-      </>
-     
+  return (  
+    <Wrapper>
+      <Hello name='react' color='red' isSpecial={true} />
+      <Hello  color='pink' />
+    </Wrapper>
   );
 }
 
