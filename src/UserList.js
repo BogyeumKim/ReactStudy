335d3e088692a982576c9112df1389a7dbca36c5
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2021-06-09 13:08:47 +0900

    useEffect
    컴포넌트가 특정 이벤트 생길때 사용

--- src/UserList.js
+++ src/UserList.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React , { useEffect } from 'react';
 
 function User({aaa, onRemove, onToggle}){
 
     const { username, email, id, active} = aaa;
+    useEffect(()=>{
+        console.log('컴포넌트가 화면에 나타남');
+        return() => {
+            console.log('컴포넌트가 화면에서 사라짐')
+        }
+    }, []);
 
     return (
         <div>
