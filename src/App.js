d4ecf29d195b000aed13c4c857cd3b8b59ac0032
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2021-06-09 13:48:17 +0900

    함수형업데이트
    useState쓸떄처럼
    
    setUsers에 users=> 로 재활용 사용

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ const onCreate = useCallback(() => {
   }
 
   // setUsers([...users,user]);
-  setUsers(users.concat(user));
+  setUsers(users=>users.concat(user));
 
   setInputs({
     username:'',
@@ -66,16 +66,16 @@ const onCreate = useCallback(() => {
   console.log(nextId.current); // 현재값 출력
   nextId.current += 1; // onCreate가 실행될떄마다 출력하고 1씩더함
   // UseRef 쓰는이유는 실행마다 nextId값을 랜더링(컴포넌트를) 다시 할필요가 없기때문에 ( ajax같은느낌 ? )
-},[username,email,users])
+},[username,email])
 
 const onRemove = useCallback( id => { 
-  setUsers(users.filter(user=>user.id !== id)); // 만족하는경우엔 넣음
+  setUsers(users=>users.filter(user=>user.id !== id)); // 만족하는경우엔 넣음
   // id 3번 삭제누르면 3번만 걸리니까 id 1,2번만 가지고 다시 배열을 생성함
-},[users]);
+},[]);
 
 
 const onToggle =  useCallback( id => {
-  setUsers(users.map(
+  setUsers(users=>users.map(
     user=>user.id === id ? 
     {
       ...user, 
@@ -84,7 +84,7 @@ const onToggle =  useCallback( id => {
      : user
   ));
 
-},[users]);
+},[]);
 
 // 특정값 업데이트할때는 ... user 기존의 useres를 수정하는게아니라 새로운 user에 넣고 덮어씌우기 map 
 // 제거할때는 filter 
